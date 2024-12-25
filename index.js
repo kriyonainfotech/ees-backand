ffb76ec9cd6c65574c1d2ba8fc77e61ac4a3638f
kriyonainfotech <kriyonainfotech@gmail.com>
2024-12-25 15:47:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
-const port = process.env.PORT || 1000;
+const port = process.env.PORT || 80;
 connectDB()
 dotenv.config()
 const bodyParser = require("body-parser");
